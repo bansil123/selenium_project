2433a9234eea00e86fa2973747a11941a8c0ce86
Bansil Patel <105497726+bansil123@users.noreply.github.com>
2024-01-26 17:09:03 -0500

    GrossPayCalculator.java file added

--- java_learning_course/src/calculator/GrossPayCalculator.java
+++ java_learning_course/src/calculator/GrossPayCalculator.java
@@ -1,2 +1,23 @@
-package calculator;public class GrossPayCalculator {
+package calculator;
+public class GrossPayCalculator {
+
+    public static void main(String args[]){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Get the number of hours worked in a week.
+        System.out.print("Please enter number of hours worked in a week: ");
+        double numOfHours = scanner.nextDouble();
+
+        //Get hourly pay rate
+        System.out.println("Please enter hourly pay rate: ");
+        double hourlyPayRate = scanner.nextDouble();
+
+        //Close the scanner.
+        scanner.close();
+
+        //Calculate and print the Gross Pay.
+        double grossPay = numOfHours * hourlyPayRate;
+        System.out.println("Weekly Gross pay for the employee is: $ " + grossPay);
+    }
 }
